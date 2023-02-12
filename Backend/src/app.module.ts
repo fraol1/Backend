e3e95657b82e14573e600b4019bbb7eb82bf1a77
Basliel Amsalu <119809588+Basliel-Amsalu@users.noreply.github.com>
2023-02-12 22:03:52 +0300

    Add files via upload

---
+++ Backend/src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { User } from './user/entity/user.entity';
+import { UserModule } from './user/user.module';
+import { ProfileModule } from './profile/profile.module';
+import { WatchListModule } from './watch-list/watchList.module';
+
+@Module({
+  controllers: [AppController],
+  imports: [
+    WatchListModule,
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User],
+      synchronize: true,
+    }),
+    AuthModule,
+    ProfileModule,
+  ],
+})
+export class AppModule {}
